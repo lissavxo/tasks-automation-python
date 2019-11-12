ba9f3ed38f24728eea78c5adf823c85d92302dfa
Lissa Victoria Ximenes de Oliveira <lissa.oliveira@compline.com.br>
2019-11-12 10:27:24 -0300

    including a getting started example

--- planilha.py
+++ planilha.py
@@ -7,20 +7,29 @@ print("start")
 
 
 program_path = r"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
-file_path    = r"C:\Users\lissa.oliveira\Documents\devDir\excel-table-example\planilha_teste.xlsx"
+file_path    = r"C:\Users\lissa.oliveira\Documents\devDir\excel-table-example\planilha-teste.xlsx"
 
 app = Application().start(r'{} "{}"'.format(program_path, file_path))
 
-screen = app.window(title='planilha_teste')
-print(screen)
+table = app.window(title="‪planilha-teste‬  -  Excel")
+table.print_control_identifiers()
 
-#read that https://jpereiran.github.io/articles/2019/06/14/Excel-automation-with-pywin32.html
 
-# wb = xlrd.open_workbook(file_path) 
-# sheet = wb.sheet_by_index(0) 
+# screen.pywinauto.mouse.click(button='left', coords=(2979,265))
+# # screen.pywinauto.mouse.scroll(coords=(3045,370), wheel_dist=1)
+
+# # screen.Pane.wait
+# # print(screen)
+
+# #read that https://jpereiran.github.io/articles/2019/06/14/Excel-automation-with-pywin32.html
+
+# # wb = xlrd.open_workbook(file_path) 
+# # sheet = wb.sheet_by_index(0) 
   
-# # For row 0 and column 0 
-# sheet.cell_value(0, 0) 
+# # # For row 0 and column 0 
+# # sheet.cell_value(0, 0) 
   
-# for i in range(sheet.ncols): 
-#     print(type(sheet.cell_value(0, i))) 
+# # for i in range(sheet.ncols): 
+# #     print(type(sheet.cell_value(0, i))) 
+
+
